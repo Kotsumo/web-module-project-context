cbac72aa7988f32491b486e1935cfd6f26611754
David <davidellis1020@gmail.com>
2021-03-23 14:02:41 -0700

    replaced props with context objects in shopping cart, navigation, and products

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 
+// contexts
+import {ProductContext} from './contexts/ProductContext'
+import {CartContext} from './contexts/CartContext'
+
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
@@ -11,22 +15,28 @@ function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
-	const addItem = item => {
+	const addItem = (item) => {
 		// add the given item to the cart
+		setCart([...cart, item]);
 	};
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
+			<CartContext.Provider value={{cart}}>
+				<Navigation cart={cart} />
 
 			{/* Routes */}
+			<ProductContext.Provider value={{products, addItem}}>
+				
 			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
+				<Products/>
 			</Route>
 
 			<Route path="/cart">
-				<ShoppingCart cart={cart} />
+				<ShoppingCart />
 			</Route>
+			</ProductContext.Provider>
+			</CartContext.Provider>
 		</div>
 	);
 }
